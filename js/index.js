a51586a1e44175393ab2d58510b78f570409f6c8
Carla Vergara <vergaracarlar@gmail.com>
2023-11-16 19:45:10 -0300

    pre entrega 2

--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
 // Definición de productos
 const productos = [
-    { id: 1, nombre: 'Producto 1', precio: 20 },
-    { id: 2, nombre: 'Producto 2', precio: 30 },
-    { id: 3, nombre: 'Producto 3', precio: 25 }
+    { id: 1, nombre: 'Producto 1', precio: 20, stock: 10 },
+    { id: 2, nombre: 'Producto 2', precio: 30, stock: 15 },
+    { id: 3, nombre: 'Producto 3', precio: 25, stock: 8 }
   ];
 
   // Función para agregar un producto al carrito
   const agregarAlCarrito = (carrito, producto) => {
-    carrito.push(producto);
-    console.log(`Se ha agregado ${producto.nombre} al carrito.`);
+    if (producto.stock > 0) {
+      carrito.push(producto);
+      producto.stock--;
+      console.log(`Se ha agregado ${producto.nombre} al carrito.`);
+    } else {
+      console.log(`El producto ${producto.nombre} está agotado.`);
+    }
   };
 
- // Función para calcular el total del carrito
-  const calcularTotal = (carrito) => {
+  // Función para calcular el total del carrito
+  const calcularTotalCarrito = (carrito) => {
     return carrito.reduce((total, producto) => total + producto.precio, 0);
+  };
+
+  // Función para calcular el total del stock de la lista de productos
+  const calcularTotalStock = (productos) => {
+    return productos.reduce((total, producto) => total + producto.stock, 0);
   };
 
   // Función para mostrar el carrito
@@ -22,5 +32,32 @@ const productos = [
     carrito.forEach(producto => {
       console.log(`${producto.nombre} - $${producto.precio}`);
     });
-    console.log(`Total: $${calcularTotal(carrito)}`);
+    console.log(`Total del carrito: $${calcularTotalCarrito(carrito)}`);
   };
+
+  // Función para mostrar el total del stock de la lista de productos
+  const mostrarTotalStock = (productos) => {
+    console.log(`Total del stock: ${calcularTotalStock(productos)}`);
+  };
+
+  // Mostrar stock inicial
+  console.log('Stock Inicial:');
+  productos.forEach(producto => {
+    console.log(`${producto.nombre} - Stock: ${producto.stock}`);
+  });
+
+  // Preguntar al usuario qué producto agregar al carrito
+  const productoSeleccionadoId = prompt('Ingrese el ID del producto que desea agregar al carrito:');
+  const productoSeleccionado = productos.find(producto => producto.id == productoSeleccionadoId);
+
+  // Verificar si el producto existe
+  if (productoSeleccionado) {
+    agregarAlCarrito(carritoDeCompras, productoSeleccionado);
+  } else {
+    console.log('Producto no encontrado.');
+  }
+
+  // Mostrar carrito y stock después de agregar el producto
+  mostrarCarrito(carritoDeCompras);
+  mostrarTotalStock(productos);
+
