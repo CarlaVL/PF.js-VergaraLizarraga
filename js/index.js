66751455cc28803f048108258a568081b784afec
Carla Vergara <vergaracarlar@gmail.com>
2023-11-16 19:56:41 -0300

    cambios en carrito

--- js/index.js
+++ js/index.js
@@ -1,18 +1,27 @@
 // Definición de productos
 const productos = [
-    { id: 1, nombre: 'Producto 1', precio: 20, stock: 10 },
-    { id: 2, nombre: 'Producto 2', precio: 30, stock: 15 },
-    { id: 3, nombre: 'Producto 3', precio: 25, stock: 8 }
+    { id: "Banana", nombre: 'Banana', precio: 50, stock: 10 },
+    { id: "Manzana", nombre: 'Manzana', precio: 30, stock: 15 },
+    { id: "Pera", nombre: 'Pera', precio: 80, stock: 10 },
+    { id: "Uvas", nombre: 'Uvas', precio: 150, stock: 7 },
+    { id: "Naranja", nombre: 'Naranja', precio: 120, stock: 36 },
+    { id: "Kiwi", nombre: 'Kiwi', precio: 200, stock: 4 },
+    { id: "Frutilla", nombre: 'Frutilla', precio: 90, stock: 23 },
   ];
 
-  // Función para agregar un producto al carrito
-  const agregarAlCarrito = (carrito, producto) => {
-    if (producto.stock > 0) {
-      carrito.push(producto);
-      producto.stock--;
-      console.log(`Se ha agregado ${producto.nombre} al carrito.`);
+  // Carrito de compras
+  const carritoDeCompras = [];
+
+  // Función para agregar una cantidad específica de un producto al carrito
+  const agregarCantidadAlCarrito = (carrito, producto, cantidad) => {
+    if (producto.stock >= cantidad) {
+      for (let i = 0; i < cantidad; i++) {
+        carrito.push({ id: producto.id, nombre: producto.nombre, precio: producto.precio });
+        producto.stock--;
+      }
+      console.log(`Se han agregado ${cantidad} unidades de ${producto.nombre} al carrito.`);
     } else {
-      console.log(`El producto ${producto.nombre} está agotado.`);
+      console.log(`No hay suficiente stock disponible para agregar ${cantidad} unidades de ${producto.nombre}.`);
     }
   };
 
@@ -46,18 +55,40 @@ const productos = [
     console.log(`${producto.nombre} - Stock: ${producto.stock}`);
   });
 
-  // Preguntar al usuario qué producto agregar al carrito
-  const productoSeleccionadoId = prompt('Ingrese el ID del producto que desea agregar al carrito:');
-  const productoSeleccionado = productos.find(producto => producto.id == productoSeleccionadoId);
+  // Bucle para agregar productos al carrito
+  let agregarMasProductos = true;
+  while (agregarMasProductos) {
+    // Preguntar al usuario qué producto agregar al carrito
+    const productoSeleccionadoId = prompt('Ingrese el ID del producto que desea agregar al carrito (o escriba "salir" para terminar):');
 
-  // Verificar si el producto existe
-  if (productoSeleccionado) {
-    agregarAlCarrito(carritoDeCompras, productoSeleccionado);
-  } else {
-    console.log('Producto no encontrado.');
+    // Verificar si el usuario desea salir del bucle
+    if (productoSeleccionadoId.toLowerCase() === 'salir') {
+      agregarMasProductos = false;
+    } else {
+      // Buscar el producto seleccionado
+      const productoSeleccionado = productos.find(producto => producto.id == productoSeleccionadoId);
+
+      // Verificar si el producto existe
+      if (productoSeleccionado) {
+        // Preguntar al usuario cuántas unidades agregar al carrito
+        const cantidadSeleccionada = parseInt(prompt(`Ingrese la cantidad de ${productoSeleccionado.nombre} que desea agregar al carrito:`), 10);
+
+        // Verificar si la cantidad ingresada es válida
+        if (!isNaN(cantidadSeleccionada) && cantidadSeleccionada > 0) {
+          agregarCantidadAlCarrito(carritoDeCompras, productoSeleccionado, cantidadSeleccionada);
+          // Mostrar carrito y stock después de agregar cada producto
+          mostrarCarrito(carritoDeCompras);
+          mostrarTotalStock(productos);
+        } else {
+          console.log('La cantidad ingresada no es válida.');
+        }
+      } else {
+        console.log('Producto no encontrado.');
+      }
+    }
   }
 
-  // Mostrar carrito y stock después de agregar el producto
+  // Mostrar carrito y stock después de salir del bucle
+  console.log('Carrito final:');
   mostrarCarrito(carritoDeCompras);
   mostrarTotalStock(productos);
-
